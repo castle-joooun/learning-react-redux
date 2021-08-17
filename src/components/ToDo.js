c0e2df91a15eb662a234f1e46a3bab6a329a5195
castle <tjdwns0309@gmail.com>
2021-08-17 18:11:54 +0900

    기능추가: redux-toolkit 적용중

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -6,16 +6,15 @@ import { actionCreators } from '../store'
 const ToDo = ({ text, onClickButton, id }) => {
   return (
     <li>
-      <Link to={`/${id}`}>
-        {text} <button onClick={onClickButton}>DEL</button>
-      </Link>
+      <Link to={`/${id}`}>{text}</Link>
+      <button onClick={onClickButton}>DEL</button>
     </li>
   )
 }
 
 const mapDispatchToProps = (dispatch, ownProps) => {
   return {
-    onClickButton: () => dispatch(actionCreators.deleteToDo(ownProps.id))
+    onClickButton: () => dispatch(actionCreators.deleteToDo(ownProps.id)),
   }
 }
 
